890a25a6ddd8e9a5cb8ff9489057dc6db93e8153
JMViiUDEZz <jmviudezp2003@gmail.com>
2022-05-25 14:20:27 +0200

    [documentacion final app marvel 3T]

--- Angular/2022/jmvpMarvelApp/src/environments/environment.ts
+++ Angular/2022/jmvpMarvelApp/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  PublicKey: '399252e317d7557e0a22b326084ab614',
-  PrivateKey: '337490d6e54fd24eb9b5e52df39966e426a07b90',
+  PublicKey: '3d016bc7da51feca88cb81f7fdfca245',
+  PrivateKey: 'd3ce7332b8d9ed2a3b8a8c09750a60921bb46e46',
   MarvelEndpoint: 'https://gateway.marvel.com/v1/public'
 };
 
